6840cbdef2b57569f9a4bd3e5f1b523d1a8ba019
asmaquraishi <asmaq66@gmail.com>
2019-02-03 19:35:09 +0300

    HW done.

---
+++ starter-code/js/app.js
@@ -0,0 +1,7 @@
+
+$(function(){
+    $('header nav > ul > li:nth-child(4) ul li:nth-child(1)').on('click', function (event){
+event.preventDefault();
+$(this).siblings().slideToggle();
+    }) 
+});
